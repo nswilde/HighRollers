f6361c9eae1399af54298230c8cb9239096a97f6
[Nswilde] <142635621+nswilde@users.noreply.github.com>
2023-11-22 10:29:24 +1300

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Nikki Wilde on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
